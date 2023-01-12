2b41ac6ba4b4ea1d2e4016480d46b3deb18f53ce
StefKoum <skoumentakeas@gmail.com>
2023-01-12 15:22:33 +0200

    more stuff

--- Session-04/Session-04/Program.cs
+++ Session-04/Session-04/Program.cs
@@ -15,7 +15,6 @@ double numberA = 50;
 double numberB = 80;
 int sum = (int) (numberA + numberB);
 double division = numberB / numberA;
-
 Console.WriteLine("The sum of the two numbers is: " + numberA + " + " + numberB + " = " + sum);
 Console.WriteLine("The division of the two numbers is: " + numberB + " / " + numberA + " = " + division);
 
@@ -38,6 +37,38 @@ double numerator = Math.Pow(6, 4) + Math.Pow(5, 7);
 Console.WriteLine("5th Operation = " + (  numerator / (9 % 4) )  );
 
 
+Console.WriteLine("\n4. Write a C# Program that assigns an age (number) and a gender (string)\n   and displays something like \"You are female and look younger than 20.\" ");
+
+
+// 4. Write a C# Program that assigns an age (number) and a gender (string)
+//    and displays something like "You are female and look younger than 20."
+string gender = "female";
+int age = 20;
+Console.WriteLine("You are " + gender + " and look younger than " + age );
+
+
+Console.WriteLine("\n5. Write a C# Program that takes an integer representing seconds (e.g. 45678) and converts it to:");
+Console.WriteLine("  -Minutes, \n  -Hours, \n  -Days");
+
+// 5. Write a C# Program that takes an integer representing seconds and converts it to:"
+// - Minutes,
+// - Hours,
+// - Days,
+
+int seconds = 129600;
+int seconds2 = seconds;
+
+int days = seconds / ((60 * 60) * 24);
+seconds %= ((60 * 60) * 24);
+
+int hours = seconds / (60 * 60);
+seconds %= (60 * 60);
+
+int minutes = seconds / 60;
+seconds %= (60 * 60);
+
+
+Console.WriteLine(seconds2 + " seconds are equivalent to: " + days + " day(s) " + hours + " hours " + minutes + " minutes " + seconds + " seconds");
 
 Console.ReadLine();
 
