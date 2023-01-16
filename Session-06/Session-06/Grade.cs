8839863b52dd97c0cffdfadc5dbf20b79aa31799
StefKoum <skoumentakeas@gmail.com>
2023-01-16 13:41:39 +0200

    session 06 test

---
+++ Session-06/Session-06/Grade.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Grade
+{
+	public Guid Id { get; set; }
+	public Guid StudentId { get; set; }
+	public Guid CourseId { get; set; }
+	public int Gradee { get; set; }
+
+}
