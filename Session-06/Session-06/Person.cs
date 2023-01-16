96c5b7f1f47a3f01b12d2c4be4165fa75471070a
StefKoum <skoumentakeas@gmail.com>
2023-01-16 17:33:50 +0200

    Person constructors

--- Session-06/Session-06/Person.cs
+++ Session-06/Session-06/Person.cs
@@ -3,9 +3,31 @@
 public class Person
 {
     //Properties
-	public Guid Id { get; set; }
+	public Guid ID { get; set; }
 	public string? Name { get; set; }
 	public int Age { get; set; }
+
+    //Constructors
+    public Person()
+    { 
+    }
+
+    public Person(Guid id)
+    {
+        ID = id;
+    }
+
+    public Person(Guid id, string name)
+    {
+        ID = id;
+        Name = name;
+    }
+    public Person(Guid id, string name, int age)
+    {
+        ID = id;
+        Name = name;
+        Age = age;
+    }
 
     //Methods
     public void GetName()
