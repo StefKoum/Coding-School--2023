415680a6de3dbf5604c93f3cd3ff58a044f049fc
StefKoum <skoumentakeas@gmail.com>
2023-01-16 13:53:41 +0200

    initial commit

---
+++ Session-06/Session-06/Course.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Course
+{
+    public Guid Id { get; set; }
+    public string? Code { get; set; }
+    public string? Subject { get; set; }
+}
+
---
+++ Session-06/Session-06/Schedule.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Schedule
+{
+    public Guid Id { get; set; }
+    public Guid CourseId { get; set; }
+    public Guid ProfessorId { get; set; }
+    public DateTime Calendar { get; set; }
+}
